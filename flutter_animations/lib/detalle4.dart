50ae88d12883f64bc5aa01f96df8d8e438266abf
AntVent33 <reynaldoantventura33@gmail.com>
2021-11-21 20:27:14 -0600

    VersionDefinitiva

---
+++ flutter_animations/lib/detalle4.dart
@@ -0,0 +1,73 @@
+// ignore_for_file: camel_case_types, avoid_unnecessary_containers
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class detalle4Page extends StatefulWidget {
+  const detalle4Page({Key? key}) : super(key: key);
+
+  @override
+  _detalle4Page createState() => _detalle4Page();
+}
+
+class _detalle4Page extends State<detalle4Page> {
+  final dur = const Duration(milliseconds: 1000);
+  var isOn = false;
+  var curve = Curves.elasticInOut;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('detalle'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              child: Hero(
+                tag: 'det4',
+                child: Image.asset(
+                  'assets/cebolla.png',
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie dolor a libero luctus mollis. Donec interdum pellentesque ipsum.',
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  isOn = !isOn;
+                });
+              },
+              child: AnimatedContainer(
+                  duration: dur,
+                  width: 75,
+                  height: 25,
+                  color: isOn ? Colors.green : Colors.blue[400],
+                  child: AnimatedAlign(
+                    duration: dur,
+                    alignment:
+                        isOn ? Alignment.centerRight : Alignment.centerLeft,
+                    curve: curve,
+                    child: AnimatedContainer(
+                        duration: dur,
+                        width: 20,
+                        height: 24,
+                        color: isOn ? Colors.purple[900] : Colors.red),
+                  )),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                'Agregar al carrito.',
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+          ],
+        ));
+  }
+}
